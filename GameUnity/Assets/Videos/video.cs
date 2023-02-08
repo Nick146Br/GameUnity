0dae1dfc9acc52cd7d4b479bb60b09be64618abf
toki-debug <toki.debug@gmail.com>
2023-02-08 17:46:51 -0300

    Credit Scene Added

---
+++ GameUnity/Assets/Videos/video.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class video : MonoBehaviour
+{
+    public GameObject tks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double video_time = GetComponent<VideoPlayer>().time;
+        Debug.Log(video_time);
+        if(video_time >124.5){
+            tks.transform.Find("Panel").gameObject.SetActive(true);
+        }else if(video_time>=157){
+            SceneManager.LoadScene("Lobby");
+        }
+        
+        
+    }
+}
